6b5399598405cc8a2c5d58668b60fc4db968f950
RabiaShah <37036029+RabiaShah@users.noreply.github.com>
2018-05-14 12:55:36 +0500

    naming convention + changes

--- Term Project 1/Project/LoadImg.Designer.cs
+++ Term Project 1/Project/LoadImg.Designer.cs
@@ -29,78 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnDetails = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.imageBox2 = new Emgu.CV.UI.ImageBox();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblFacesDetected = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.pbBrowsedImage = new Emgu.CV.UI.ImageBox();
+            this.pbDetectedFace = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrowsedImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDetectedFace)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnDetails
+            // btnBrowse
             // 
-            this.btnDetails.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnDetails.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnDetails.FlatAppearance.BorderSize = 2;
-            this.btnDetails.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.btnDetails.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDetails.ForeColor = System.Drawing.Color.MediumSpringGreen;
-            this.btnDetails.Location = new System.Drawing.Point(231, 356);
-            this.btnDetails.Name = "btnDetails";
-            this.btnDetails.Size = new System.Drawing.Size(95, 30);
-            this.btnDetails.TabIndex = 5;
-            this.btnDetails.Text = "Browse";
-            this.btnDetails.UseVisualStyleBackColor = false;
-            this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBrowse.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnBrowse.FlatAppearance.BorderSize = 2;
+            this.btnBrowse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnBrowse.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnBrowse.Location = new System.Drawing.Point(231, 356);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(95, 30);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnDetails_Click);
             // 
-            // label1
+            // lblFacesDetected
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.label1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Aqua;
-            this.label1.Location = new System.Drawing.Point(550, 105);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 24);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Face Detected";
+            this.lblFacesDetected.AutoSize = true;
+            this.lblFacesDetected.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblFacesDetected.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFacesDetected.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblFacesDetected.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFacesDetected.ForeColor = System.Drawing.Color.Aqua;
+            this.lblFacesDetected.Location = new System.Drawing.Point(550, 105);
+            this.lblFacesDetected.Name = "lblFacesDetected";
+            this.lblFacesDetected.Size = new System.Drawing.Size(155, 24);
+            this.lblFacesDetected.TabIndex = 6;
+            this.lblFacesDetected.Text = "Face Detected";
             // 
-            // button1
+            // btnCheck
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.button1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.MediumSpringGreen;
-            this.button1.Location = new System.Drawing.Point(469, 397);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 46);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Check Record";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCheck.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCheck.FlatAppearance.BorderSize = 2;
+            this.btnCheck.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnCheck.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnCheck.Location = new System.Drawing.Point(469, 397);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(154, 46);
+            this.btnCheck.TabIndex = 8;
+            this.btnCheck.Text = "Check Record";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.button1_Click);
             // 
-            // imageBox1
+            // pbBrowsedImage
             // 
-            this.imageBox1.Location = new System.Drawing.Point(231, 106);
-            this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(284, 244);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox1.TabIndex = 2;
-            this.imageBox1.TabStop = false;
+            this.pbBrowsedImage.Location = new System.Drawing.Point(231, 106);
+            this.pbBrowsedImage.Name = "pbBrowsedImage";
+            this.pbBrowsedImage.Size = new System.Drawing.Size(284, 244);
+            this.pbBrowsedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBrowsedImage.TabIndex = 2;
+            this.pbBrowsedImage.TabStop = false;
             // 
-            // imageBox2
+            // pbDetectedFace
             // 
-            this.imageBox2.Location = new System.Drawing.Point(550, 132);
-            this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(155, 162);
-            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox2.TabIndex = 2;
-            this.imageBox2.TabStop = false;
+            this.pbDetectedFace.Location = new System.Drawing.Point(550, 132);
+            this.pbDetectedFace.Name = "pbDetectedFace";
+            this.pbDetectedFace.Size = new System.Drawing.Size(155, 162);
+            this.pbDetectedFace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDetectedFace.TabIndex = 2;
+            this.pbDetectedFace.TabStop = false;
             // 
             // LoadImg
             // 
@@ -108,16 +108,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Project.Properties.Resources._31061468_976249289209152_3606521597940203520_n__1_;
             this.ClientSize = new System.Drawing.Size(947, 487);
-            this.Controls.Add(this.imageBox2);
-            this.Controls.Add(this.imageBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnDetails);
+            this.Controls.Add(this.pbDetectedFace);
+            this.Controls.Add(this.pbBrowsedImage);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblFacesDetected);
+            this.Controls.Add(this.btnBrowse);
             this.Name = "LoadImg";
             this.Text = "Check Record";
             this.Load += new System.EventHandler(this.LoadImg_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrowsedImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDetectedFace)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,10 +125,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnDetails;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private Emgu.CV.UI.ImageBox imageBox1;
-        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblFacesDetected;
+        private System.Windows.Forms.Button btnCheck;
+        private Emgu.CV.UI.ImageBox pbBrowsedImage;
+        private Emgu.CV.UI.ImageBox pbDetectedFace;
     }
 }
--- Term Project 1/Project/PictureComparison.Designer.cs
+++ Term Project 1/Project/PictureComparison.Designer.cs
@@ -30,13 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PictureComparison));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pbCpaturedImg = new System.Windows.Forms.PictureBox();
+            this.pbDBImage = new System.Windows.Forms.PictureBox();
+            this.lblFound = new System.Windows.Forms.Label();
             this.btnDetails = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCpaturedImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDBImage)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -47,32 +47,33 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // pbCpaturedImg
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(218, 96);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(266, 220);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pbCpaturedImg.Location = new System.Drawing.Point(218, 96);
+            this.pbCpaturedImg.Name = "pbCpaturedImg";
+            this.pbCpaturedImg.Size = new System.Drawing.Size(266, 220);
+            this.pbCpaturedImg.TabIndex = 1;
+            this.pbCpaturedImg.TabStop = false;
             // 
-            // pictureBox3
+            // pbDBImage
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(510, 96);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(266, 220);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pbDBImage.Location = new System.Drawing.Point(510, 96);
+            this.pbDBImage.Name = "pbDBImage";
+            this.pbDBImage.Size = new System.Drawing.Size(266, 220);
+            this.pbDBImage.TabIndex = 2;
+            this.pbDBImage.TabStop = false;
+            this.pbDBImage.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
-            // label1
+            // lblFound
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(232, 337);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(575, 32);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Match Found/ No Record Found";
+            this.lblFound.AutoSize = true;
+            this.lblFound.BackColor = System.Drawing.Color.Transparent;
+            this.lblFound.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFound.Location = new System.Drawing.Point(232, 337);
+            this.lblFound.Name = "lblFound";
+            this.lblFound.Size = new System.Drawing.Size(575, 32);
+            this.lblFound.TabIndex = 3;
+            this.lblFound.Text = "Match Found/ No Record Found";
             // 
             // btnDetails
             // 
@@ -97,17 +98,17 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(920, 487);
             this.Controls.Add(this.btnDetails);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblFound);
+            this.Controls.Add(this.pbDBImage);
+            this.Controls.Add(this.pbCpaturedImg);
             this.Controls.Add(this.pictureBox1);
             this.Name = "PictureComparison";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PictureComparison";
             this.Load += new System.EventHandler(this.PictureComparison_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCpaturedImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDBImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,9 +117,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbCpaturedImg;
+        private System.Windows.Forms.PictureBox pbDBImage;
+        private System.Windows.Forms.Label lblFound;
         private System.Windows.Forms.Button btnDetails;
     }
 }
